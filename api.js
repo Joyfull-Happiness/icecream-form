82768cc1e5a8eef1752a6f72912393de097ad99f
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-04-22 09:29:32 -0700

    updated js

--- api.js
+++ api.js
@@ -12,12 +12,20 @@ function onFormSubmit() {
 
   form.style.display = "none";
 
+  let name = dataObject.name;
+
+  let message = `Thank you ${name} for your order!`;
+  console.log("first message to user:", message);
   // message.style.display = "none";
   // create a new element <p></p>
   // add a message
   // add a message within the paragraph
   // add a class to this paragraph element using class list
   // add your style to the class (in css or js)
+
+  let messageParagraph = document.createElement("p");
+  messageParagraph.textContent = message;
+  container.appendChild(messageParagraph);
 
   let customerOrders = [];
 
